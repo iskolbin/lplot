921160c80ec572e0a57babed21a7ac32570deeda
Ilya Kolbin <iskolbin@gmail.com>
2017-02-22 18:31:31 +0300

    refactorings; upd lic

--- plot.lua
+++ plot.lua
@@ -1,15 +1,33 @@
--- Lua implementation of simple rasterization algorithms. 
--- 
--- Based mainly on C-implementation by Zingl Alois, 
--- see http://members.chello.at/~easyfilter/bresenham.html. 
---
--- All presented functions take a plotting function f as the first argument. 
--- Algorithms pass 3 parameters to a plotting function:
--- x and y coordinates and alpha value ( 0 -- minimum, 1 - maximum ), 
--- for not antialiased algorithms alpha value is always equals 1.
--- If plotting function return non-false result then algorithm will halt.
---
--- coded by Ilya Kolbin ( iskolbin@gmail.com )
+--[[
+
+ plot -- v0.2.0 public domain Lua rasterization algorithms
+ no warranty implied; use at your own risk
+
+ author: Ilya Kolbin (iskolbin@gmail.com)
+ url: github.com/iskolbin/plot
+
+ Lua implementation of simple rasterization algorithms.
+
+ Based mainly on C-implementation by Zingl Alois,
+ see http://members.chello.at/~easyfilter/bresenham.html.
+
+ All presented functions take a plotting function f as the first argument.
+ Algorithms pass 3 parameters to a plotting function:
+ x and y coordinates and alpha value ( 0 -- minimum, 1 - maximum ),
+ for not antialiased algorithms alpha value is always equals 1.
+ If plotting function return non-false result then algorithm will halt.
+
+ COMPATIBILITY
+
+ Lua 5.1, 5.2, 5.3, LuaJIT 1, 2
+
+ LICENSE
+
+ This software is dual-licensed to the public domain and under the following
+ license: you are granted a perpetual, irrevocable license to copy, modify,
+ publish, and distribute this file as you see fit.
+
+--]]
 
 local abs, floor = math.abs, math.floor
 
@@ -20,14 +38,14 @@ local plot = {}
 function plot.line( f, x0, y0, x1, y1 )
 	local dx, sx = abs( x1 - x0 ), x0 < x1 and 1 or -1
 	local dy, sy = -abs( y1 - y0 ), y0 < y1 and 1 or -1
-	local err, e2 = dx + dy, 0
+	local err = dx + dy
 
 	while true do	
 		f( x0, y0, 1 )
 		if x0 == x1 and y0 == y1 then
 			break
 		else
-			e2 = err + err
+			local e2 = err + err
 			if e2 >= dy then
 				err, x0 = err + dy, x0 + sx
 			end
@@ -42,14 +60,14 @@ end
 -- Midpoint circle algorithm
 -- see https://en.wikipedia.org/wiki/Midpoint_circle_algorithm
 function plot.circle( f, x0, y0, r )
-	local x, y, err = -r, 0, 2 - 2*r -- II. Quadrant 
+	local x, y, err = -r, 0, 2 - 2*r -- II. Quadrant
 	repeat
 		if f( x0 - x, y0 + y, 1 ) -- I. Quadrant
 		or f( x0 - y, y0 - x, 1 ) -- II. Quadrant
 		or f( x0 + x, y0 - y, 1 ) -- III. Quadrant
 		or f( x0 + y, y0 + x, 1 ) -- IV. Quadrant
-		then 
-			return 
+		then
+			return
 		end
 
 		r = err
@@ -63,17 +81,17 @@ function plot.circle( f, x0, y0, r )
 			x = x + 1
 			err = err + x + x + 1 -- e_xy+e_x > 0 or no 2nd y-step
 		end
-	until x > 0 
+	until x > 0
 end
 
 function plot.ellipse( f, x0, y0, x1, y1 )
 	local a, b = abs( x1-x0 ), abs( y1-y0 ) -- values of diameter
 	local b1 = b % 2
 	local dx, dy = 4*(1-a)*b*b, 4*(b1+1)*a*a -- error increment
-	local err, e2 = dx+dy+b1*a*a, 0 --
+	local err = dx+dy+b1*a*a
 
-	if x0 > x1 then 
-		x0 = x1 
+	if x0 > x1 then
+		x0 = x1
 		x1 = x1 + a
 	end -- if called with swapped points
 
@@ -81,9 +99,9 @@ function plot.ellipse( f, x0, y0, x1, y1 )
 		y0 = y1 -- .. exchange them
 	end
 		
-	y0 = y0 + floor(0.5*(b + 1)) 
+	y0 = y0 + floor(0.5*(b + 1))
 	y1 = y0 - b1 -- starting pixel
-	a = 8*a*a 
+	a = 8*a*a
 	b1 = 8*b*b
 
 	repeat
@@ -95,7 +113,7 @@ function plot.ellipse( f, x0, y0, x1, y1 )
 			return
 		end
 
-		e2 = err + err
+		local e2 = err + err
 		if e2 <= dy then
 			y0 = y0 + 1
 			y1 = y1 - 1
@@ -112,7 +130,7 @@ function plot.ellipse( f, x0, y0, x1, y1 )
 
 	while y0 - y1 < b do  -- too early stop of flat ellipses a=1
 		if f( x0 - 1, y0, 1 ) -- finish tip of ellipse
-		or f( x1 + 1, y0, 1 ) 
+		or f( x1 + 1, y0, 1 )
 		or f( x0-1, y1, 1 )
 		or f( x1+1, y1, 1 )
 		then
@@ -160,7 +178,7 @@ function plot.lineaa( f, x0, y0, x1, y1 )
 				end
 			end
 			err = err + dx
-			y0 = y0 + sy 
+			y0 = y0 + sy
 		end
 	end
 end
